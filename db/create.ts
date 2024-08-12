cfce678c5ec630e2fd58b708c2b1bcb6b612574c
akshat k <akshatkot@gmail.com>
2024-08-12 23:24:43 +0530

    jump9

--- db/create.ts
+++ db/create.ts
@@ -4,13 +4,14 @@ export async function createTableDeployments(){
 	database.dbQuery(`CREATE TABLE IF NOT EXISTS deployments (
 		id SERIAL PRIMARY KEY,
 		deployment_id VARCHAR(255) NOT NULL,
-		user-name VARCHAR(255) NOT NULL,
+		user_name VARCHAR(255) NOT NULL,
 		time TIMESTAMP NOT NULL,
 		status VARCHAR(255) NOT NULL,
 		`);
 }
 
 
+
+
 createTableDeployments();
-
-
+console.log('Table created');
