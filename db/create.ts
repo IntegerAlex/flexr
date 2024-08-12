ed30e6a02bd65366611f924d87358bf837dda747
akshat k <akshatkot@gmail.com>
2024-08-12 23:30:35 +0530

    jump11

--- db/create.ts
+++ db/create.ts
@@ -1,17 +1,19 @@
 import database from './main';
 
-export async function createTableDeployments(){
-	database.dbQuery(`CREATE TABLE IF NOT EXISTS deployments (
-		id SERIAL PRIMARY KEY,
-		deployment_id VARCHAR(255) NOT NULL,
-		user_name VARCHAR(255) NOT NULL,
-		time TIMESTAMP NOT NULL,
-		status VARCHAR(255) NOT NULL,
-		`);
+export async function createTableDeployments() {
+    try {
+        await database.dbQuery(`
+            CREATE TABLE IF NOT EXISTS deployments (
+                id SERIAL PRIMARY KEY,
+                deployment_id VARCHAR(255) NOT NULL,
+                user_name VARCHAR(255) NOT NULL,
+                time TIMESTAMP NOT NULL,
+                status VARCHAR(255) NOT NULL
+            );
+        `);
+        console.log("Table 'deployments' created or already exists.");
+    } catch (error) {
+        console.error('Error creating table:', error);
+    }
 }
-
-
-
-
-
 
