ac4073b3a3998efb40665350465872e7a67bf1f5
akshat k <akshatkot@gmail.com>
2024-08-14 00:29:28 +0530

    jump1

--- db/operations.ts
+++ db/operations.ts
@@ -13,21 +13,22 @@ export async function getDeployments(userName: string) {
         throw err;
     }
 }
-export async function postDeployment(project_name:string ,userName: string, containerId: string) {
+export async function postDeployment(projectName: string, userName: string, containerId: string) {
     const query = `
-        INSERT INTO deployments (project_name,user_name, container_id,status)
-        VALUES ($1, $2,$3 , 'deployed');
+        INSERT INTO deployments (project_name, user_name, container_id, status)
+        VALUES ($1, $2, $3, 'deployed');
     `;
 
     try {
         // Use parameterized query to avoid SQL injection and syntax issues
-        await database.dbQuery(query, [userName, containerId]);
+        await database.dbQuery(query, [projectName, userName, containerId]);
         console.log("Deployment inserted successfully.");
     } catch (error) {
         console.error('Error inserting deployment:', error);
         throw error; // Re-throw the error to handle it in the calling function
     }
 }
+
 export async function deleteDeployment(userName:string, containerId:string){
 	database.dbQuery(`DELETE FROM deployments WHERE user_name = ${userName} AND container_id = ${containerId}`)
 }
